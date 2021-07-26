e65e3f225929927badc8efe165723ab8db72e1a0
Vincent Walker <macbook2021@vincents-mbp.home>
2021-07-26 17:08:50 +0100

    Gem updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create(name: 'Vincent Walker', email: 'vincethewalker@yahoo.co.uk', password: 'password', admin: true)
